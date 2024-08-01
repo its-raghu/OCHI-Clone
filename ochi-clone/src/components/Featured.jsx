61c86e964b15f2bed1ccd01fcf1c08e94b6f2fba
its-raghu <raghava.simhadri@gmail.com>
2024-08-02 03:04:47 +0530

    Featured initial

---
+++ ochi-clone/src/components/Featured.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Featured() {
+  return (
+    <div>
+        <div className='w-full bg-red-400 px-16 py-8 pt-24 font-Neue text-[3.5vw] border-b-2 border-gray-950'>Featured projects</div>
+        <div className='w-full h-screen flex gap-4'>
+            <div className='w-1/2 h-3/4 bg-green-400'></div>
+            <div>Hello</div>
+        </div>
+    </div>
+  )
+}
+
+export default Featured
