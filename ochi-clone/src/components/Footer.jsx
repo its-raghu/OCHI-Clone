f9a978c0e20cb52dedfbd65108226eb39a20ad42
its-raghu <raghava.simhadri@gmail.com>
2024-08-03 20:31:05 +0530

    updated footer

---
+++ ochi-clone/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='w-full h-screen bg-blue-300'>
+        <div className='w-1/2 h-full bg-green-400 p-20'>
+            <h1 className='uppercase text-[10vw] font-Grotesk leading-10'>eye-<br/>opening</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
